4da2641a324790010ac97afe270c009d61b8cda5
Eduardo668 <eduardosehn20@gmail.com>
2022-05-18 11:12:01 -0300

    First Commit

---
+++ Backend/src/main/java/br/com/polus/PolusDigitalOutfitApplication.java
@@ -0,0 +1,13 @@
+package br.com.polus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolusDigitalOutfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolusDigitalOutfitApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/br/com/polus/PolusDigitalOutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.polus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolusDigitalOutfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
